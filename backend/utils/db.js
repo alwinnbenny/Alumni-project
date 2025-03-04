a0a4262bbfbabd623759c09163ea75b3ec79d2dd
alwinnbenny <167227664+alwinnbenny@users.noreply.github.com>
2025-03-04 15:07:46 +0530

    database setup

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,7 +1,12 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 
 const con = mysql.createConnection({
@@ -9,9 +14,9 @@ const con = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // port: process.env.DB_PORT 
+
 })
-// "port: process.env.PORT" remove this line to run offline on xampp
+
 
 con.connect((err) => {
     if (err) {
